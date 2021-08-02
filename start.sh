9f228d881358882805a9293060f6e7663b965414
hong <lhk810@gmail.com>
2021-08-03 01:15:41 +0900

    [React] Add client codebase
    - add react src
    - change start script
    - dockerize react app

--- start.sh
+++ start.sh
@@ -1,20 +1,28 @@
-if [ $# -lt 2 ]; then
-    echo "usage: $0 MYSQL_ROOT_PASSWORD SPRING_PASSWORD"
-    exit 1
-fi
-
 PROJECT_DIR=`git rev-parse --show-toplevel`
-
-MYSQL_ROOT_PASSWORD=${1}
-SPRING_PASSWORD=${2}
-export MYSQL_ROOT_PASSWORD
-export SPRING_PASSWORD
-
-cd ${PROJECT_DIR}/report-spring
-./gradlew bootJar
-docker build -t lhk810/daily-report-spring .
-
 cd ${PROJECT_DIR}
 
-envsubst < "docker/init.sql.in" > "docker/init.sql"
-docker-compose -f docker/docker-compose.yml up -d --build
+case $1 in
+    server)
+        if [ $# -lt 3 ]; then
+            echo "usage: $0 $1 MYSQL_ROOT_PASSWORD SPRING_PASSWORD"
+            exit 1
+        fi
+
+        MYSQL_ROOT_PASSWORD=${1}
+        SPRING_PASSWORD=${2}
+        export MYSQL_ROOT_PASSWORD
+        export SPRING_PASSWORD
+
+        cd ${PROJECT_DIR}/report-spring
+        ./gradlew bootJar
+        docker build -t lhk810/daily-report-spring .
+
+        cd ${PROJECT_DIR}
+
+        envsubst < "docker/init.sql.in" > "docker/init.sql"
+        docker-compose -f docker/docker-compose-server.yml up -d --build
+        ;;
+    client)
+        docker-compose -f docker/docker-compose-client.yml up -d --build
+        ;;
+esac
