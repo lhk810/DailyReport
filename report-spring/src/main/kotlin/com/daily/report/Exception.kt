01443813cae0654c0776a47c5fdacca346f5709c
hong <lhk810@gmail.com>
2021-08-12 22:21:51 +0900

    [Spring] Add exceotion hadnler
    - Custom errors have only status and message yet

--- report-spring/src/main/kotlin/com/daily/report/Exception.kt
+++ report-spring/src/main/kotlin/com/daily/report/Exception.kt
@@ -1,5 +1,33 @@
 package com.daily.report
 
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.server.ResponseStatusException
+
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+    @ResponseBody
+    @ExceptionHandler(ResponseStatusException::class)
+    fun handleBadRequestException(ex: ResponseStatusException): Error {
+        return Error(ex.status, ex.reason)
+    }
+}
+
+class Error {
+    var status: HttpStatus? = null
+    var message: String? = null
+
+    constructor(status: HttpStatus?, message: String?) {
+        this.status = status
+        this.message = message
+    }
+}
+
 class TaskNotFoundException(): Throwable() {
 }
 
